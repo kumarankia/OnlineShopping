d651a9232b495fa06a1df5e72ca69dc61c136ff9
makum <makum@KUMARAN>
2024-03-13 18:24:15 +1300

    Add project files.

---
+++ OnlineShopping/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace OnlineShopping
+{ class Program
+    {
+         static void Main(string[] args)
+        {
+            double total,discountPrice;
+            
+            Console.WriteLine("Hey customer Your total amount of pursase : ");
+            total = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Your total amount is $ "+total);
+            if (total > 100) { discountPrice = total-total * 1 / 10;
+                Console.WriteLine("Your amount after discount $" + discountPrice);
+            } 
+            else if (total > 50) 
+                { discountPrice = total-total*5/100;
+                Console.WriteLine("Your amount after discount give $"+discountPrice);
+            }else { Console.WriteLine("You are not eligle for discount "); }
+
+
+
+
+
+
+
+
+
+        }
+
+
+
+    }
+
+
+
+
+}
